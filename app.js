0da9140bd9288859baf02fadff73f1da9ceceb28
RoushanFW196 <roushankumar6338@gmail.com>
2021-11-19 18:41:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 
-// const firstuser = require('./first');
-// console.log(firstuser())
-// const seconduser = require('./sec');
-// console.log(seconduser())
-// const thirduser = require('./thr')
-//   console.log("hello")
-//   console.log(thirduser())
 
-//  const EventEmitter= require('events');
-//  const eventEmitter = new EventEmitter();
-  
-//  eventEmitter.on("bolo",function(){
-//      console.log("listener1")
-//  })
 
-// eventEmitter.on("bolo",()=>{console.log("listener2")})
-
-// eventEmitter.emit("bolo")
 
 
  const express= require('express');
